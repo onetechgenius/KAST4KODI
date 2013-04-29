c9e19c9f2365b5738b07054007632681daf4495d
Mike C <mcrosson_git at nusku dot net>
2013-04-29 20:40:17 +0000

    Initial tweaks
    Conflicts:
    
    	xbmc/Application.cpp

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -612,6 +612,9 @@ bool CApplication::Create()
   CopyUserDataIfNeeded("special://masterprofile/", "favourites.xml");
   CopyUserDataIfNeeded("special://masterprofile/", "Lircmap.xml");
   CopyUserDataIfNeeded("special://masterprofile/", "LCD.xml");
+  CopyUserDataIfNeeded("special://masterprofile/", "playercorefactory.xml");
+  CopyUserDataIfNeeded("special://masterprofile/", "sources.xml");
+  CopyUserDataIfNeeded("special://masterprofile/", "guisettings.xml");
 
   if (!CLog::Init(CSpecialProtocol::TranslatePath(g_settings.m_logFolder).c_str()))
   {
